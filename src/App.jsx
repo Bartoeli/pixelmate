e1bbd6dd8551b9e07343c9a874813edb2fc99083
Eliška Bartošová <bartosova.eli@gmail.com>
2021-02-19 15:40:48 +0100

    router

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Detail } from './pages/detail/Detail.jsx';
+import { Homepage } from './pages/homepage/Homepage.jsx';
+import { List } from './pages/list/List.jsx';
+
+import './App.scss';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+        <Route path="/list">
+          <List />
+        </Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/menuLogo/MenuLogo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './menuLogo.scss';
+
+export const MenuLogo = () => {
+  return <></>;
+};
